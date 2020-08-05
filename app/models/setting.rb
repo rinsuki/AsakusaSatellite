b3761e81180f6dea4f6cffcfa76a273a2317bc56
rinsuki <428rinsuki+git@gmail.com>
2020-08-06 04:56:15 +0900

    settingsをSETTINGS_YAMLから動的に変更できるように

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -4,9 +4,13 @@ class Setting
       name = "#{ENV['SETTINGS'] || 'settings'}.yml"
       Rails.root.join('config', name)
     end
+
+    def settings_content
+      ENV['SETTINGS_YAML'] || File.read(settings_path)
+    end
   end
 
-  @@available_settings = YAML::load(ERB.new(File.read(settings_path)).result)
+  @@available_settings = YAML::load(ERB.new(settings_content).result)
 
   def self.[](key)
     @@available_settings[key.to_s]
